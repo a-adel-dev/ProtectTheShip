ef4d9b4a0fcc051dc0aa1d12917d197a884a2841
Ahmad Adel Fouad <a.adel.dev@gmail.com>
2023-03-25 23:35:39 +0200

    Add Health Bars

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    [SerializeField]
+    private Slider _slider;
+
+    public void SetHealth(int health)
+    {
+        _slider.value = health;
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        _slider.maxValue = health;
+    }
+
+}

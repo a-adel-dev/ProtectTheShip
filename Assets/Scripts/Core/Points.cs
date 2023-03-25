e345256039065e758bfad4f1972e2b46464af9b2
Ahmad Adel Fouad <a.adel.dev@gmail.com>
2023-03-25 22:32:48 +0200

    Add point System Start Enemy behavior

---
+++ Assets/Scripts/Core/Points.cs
@@ -0,0 +1,47 @@
+﻿
+
+using UnityEngine;
+
+namespace com.ARTillery.Core
+{
+    public class Points
+    {
+        private int _maxPoints;
+        private int _currentPoints;
+
+        public int MaxPoints { get => _maxPoints; set => _maxPoints = value; }
+        public int CurrentPoints { get => _currentPoints; set => _currentPoints = value; }
+
+        public Points(int maxPoints)
+        {
+            MaxPoints = maxPoints;
+            CurrentPoints = maxPoints;
+        }
+
+        public void ReducePoints(int value)
+        {
+            CurrentPoints = Mathf.Min( CurrentPoints - value , 0);
+        }
+
+        public void ResetPoints()
+        {
+            CurrentPoints = MaxPoints;
+        }
+
+        public void AddPoints(int value)
+        {
+            CurrentPoints = Mathf.Max(CurrentPoints + value, MaxPoints);
+        }
+
+        public int GetCurrentPoints()
+        {
+            return CurrentPoints;
+        }
+
+        public bool IsOutOfPoints()
+        {
+            return _currentPoints <= 0;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Core/StatCollection.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+namespace com.ARTillery.Core
+{
+    [CreateAssetMenu(fileName = "Stat Collection", menuName = "ScriptableObjects/Stat Collections", order = 1)]
+    public class StatCollection : ScriptableObject
+    {
+        public int Health;
+        public float Speed;
+        public int AttackPower;
+        public int Armor;
+        public float AttackRate;
+        
+    }
+}
